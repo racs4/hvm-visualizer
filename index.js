e3d63944617d02d05978a33dec89d2f9b5c736f3
rheidner <rheidner.achiles@gmail.com>
2022-04-06 14:38:57 -0300

    change serve for express

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ const { writeFileSync, readFileSync } = require("fs");
 const path = require("path");
 const exec = require("child_process").execSync;
 
+const express = require('express')
+const app = express()
+
 function main() {
   const filePath = process.argv[2];
   const debugContent = readFileSync(filePath).toString().replaceAll("${", "\\${");
 
   const gambiarraPath = path.join(__dirname, "src", "gambiarra.js");
   const gambiarraFile = readFileSync(gambiarraPath);
-  const newGambiarraContent = gambiarraFile.toString().replace(/const value = `.*`;/sm, "const value = `"+ debugContent +"`;");
+  const newGambiarraContent = gambiarraFile.toString().replace(/const value = `.*`;/sm, "const value = `" + debugContent + "`;");
 
   writeFileSync(gambiarraPath, newGambiarraContent);
 
-  console.log("Serving... in http://localhost:4000");
-  exec("npm run serve");
+  
+  app.use(express.static('src'));
+  app.listen(4000, () => {
+    console.log("Serving... in http://localhost:4000");
+  })
 }
 
 main();
